0724ccbfcdaff535c45b5675db2df1cd3207a685
rivofirdaus <41298783+rivofirdaus@users.noreply.github.com>
2018-08-14 10:57:03 +0700

    Update Kasi_model.php

--- application/modules/Kasi/models/Kasi_model.php
+++ application/modules/Kasi/models/Kasi_model.php
@@ -4,8 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kasi_model extends CI_Model
 {
-  
-
+    public $vtable_kasi = 'v_pptk';
     public function __construct()
     {
         parent::__construct();
@@ -17,43 +16,148 @@ class Kasi_model extends CI_Model
         $query = $this->db->select("*")->from("tab_pns")->order_by("nama","asc")->get();
         return $query->result_array();
     }
-	 public function getPnsE12()
+	
+	public function getpnsbyeselon($id_opd)
+	{
+		$query = $this->db->select("*")->from("v_namaeselon")->where('unitkey',$id_opd)->where('id_eselon',4)->order_by("id_eselon","asc")->get();
+        return $query->result_array();
+	}
+	
+	public function getpnskasi($id_opd)
+	{
+		$query = $this->db->select("*")->from("v_namaeselon")->where('unitkey',$id_opd)->where('id_eselon',5)->order_by("id_eselon","asc")->get();
+        return $query->result_array();
+	}
+	
+	public function getparentid($id_opd)
+	{
+		$query = $this->db->select("*")->from("v_namaeselon")->where('unitkey',$id_opd)->where('parent_id')->order_by("id_eselon","asc")->get();
+        return $query->result_array();
+	}
+	
+	public function get_pns_by_opd($id_opd)
+	{
+		$query = $this->db->select("*")->from("tab_pns")->where('unitkey',$id_opd)->order_by("nama","asc")->get();
+        return $query->result_array();
+	}
+	
+	public function get_namaunit($unitkey)
+	{
+		$query = $this->db->select("*")->from("daftunit")->where('unitkey',$unitkey)->get();
+        return $query->result_array();
+	}
+	
+	public function getkegiatan($unitkey)
     {
-        $query = $this->db->select("*")->from("v_opd")->get();
+        $query = $this->db->select("*")->from("dpa22")->where('unitkey',$unitkey)->group_by('kdkegunit')->get();
         return $query->result_array();
     }
-	 public function getopd()
+	
+	public function getnilai($unitkey)
     {
-        $query = $this->db->select("*")->from("v_opd")->group_by("nama_opd","asc")->get();
+        $query = $this->db->select_sum("nilai")->from("dpa22")->where('unitkey',$unitkey)->group_by('kdkegunit')->get();
         return $query->result_array();
     }
-	public function getunit()
+	
+	public function getjabatanuser($id)
     {
-        $query = $this->db->select("*")->from("daftunit")->group_by("nmunit","asc")->get();
+          return $this->db->select('jabatan_pns')->from('v_jabatanuser1')->where('id_pns', $id)->get()->result_array();
+    }
+	
+	public function getkegiatanpptk($id)
+    {
+        $query = $this->db->select("*")->from("tab_pptk")->where('idpnspptk',$id)->get();
         return $query->result_array();
     }
-	public function gettahun()
+	
+		//public function getkegiatanok($unitkey)
+		//{
+		//   $query = $this->db->select("*")->from("dpa22")->where('unitkey',$unitkey)->get();
+		//  return $query->result_array();
+		//}
+		//public function getnamakegok($unitkey)
+		//{
+		//  $query = $this->db->select("*")->from("mkegiatan")->where('kdkegunit',$unitkey)->get();
+		// $data = $query->result_array();
+		//	return $data[0];
+		//}
+	
+	 public function countts($id)
     {
-        $query = $this->db->select("*")->from("m_tahunanggaran")->group_by("tahun","asc")->get();
-        return $query->result_array();
+        $query = count($this->db->select('*')->from('v_pptk')->where('idpnspptk',$id)->get()->result_array());
+        return $query;
     }
-public function getmatangr()
+	 public function json($id)
+    {
+        $this->datatables->select('idpptk,nmkegunit,nilai');
+        $this->datatables->from($this->vtable_kasi);
+        $this->datatables->where('idpnspptk', $id);
+        $this->datatables->add_column('aksi', anchor(site_url('Kasi/detail/$1'), '<button class="btn1 btn btn-default btn-xs btn-block" ><i class="glyphicon glyphicon-eye-open"></i> Target Keuangan</button>'), 'idpptk');
+		 $this->datatables->add_column('aksi2', anchor(site_url('Kasi/fisik/$1'), '<button class="btn1 btn btn-default btn-xs btn-block" ><i class="glyphicon glyphicon-eye-open"></i> Target Fisik</button>'), 'idpptk');
+		  $this->datatables->add_column('aksi3', anchor(site_url('Kasi/realisasi/$1'), '<button class="btn1 btn btn-default btn-xs btn-block" ><i class="glyphicon glyphicon-eye-open"></i> Realisasi</button>'), 'idpptk');
+        return $this->datatables->generate();
+    }
+	
+	public function getnamakeg($unitkey)
+    {
+        $query = $this->db->select("*")->from("mkegiatan")->where('kdkegunit',$unitkey)->get();
+        $data = $query->result_array();
+		return $data[0];
+    }
+	
+	public function getpnsnip($id)
+	{
+		$this->db->where('id',$id);
+		$this->db->select('*');
+		$this->db->from('tab_pns');
+		$data = $this->db->get()->result_array();
+		return $data[0];
+	}
+		
+	public function getmatangr()
     {
         $query = $this->db->select("*")->from("matangr")->group_by("nmper","asc")->get();
         return $query->result_array();
     }
 	
-	 public function getidopd($id)
+	public function getmpgrm()
     {
-        $query = $this->db->select("id_opd")->from("tab_pns")->where("id",$id)->get();
-        $idopd = $query->result_array();
-        return $idopd[0];
+        $query = $this->db->select("*")->from("mpgrm")->group_by("NMPRGRM","asc")->get();
+        return $query->result_array();
     }
-	 public function getOpdPns($id)
+	
+	public function gettahun()
     {
-        $query = $this->db->select("id_opd")->from("tab_pns")->where("id",$id)->get();
-        $idopd = $query->result();
-        return $idopd[0];
+        $query = $this->db->select("*")->from("m_tahunanggaran")->get();
+        return $query->result_array();
+    }
+	
+	public function get_id_opd($id)
+	{
+		return $this->db->select('unitkey')->from('tab_pns')->where('id',$id)->get()->result_array()[0];
+	}
+	
+	
+	 public function insert_pptk($data)
+    {
+        $this->db->insert('tab_pptk',$data);
+        $id = $this->db->insert_id();
+        return $id;
     }
 
+	public function savemohon($valuesyarat)
+    {
+        $this->db->trans_start();
+        $this->db->insert_batch('tab_dok_pendukung', $valuesyarat);
+        $this->db->trans_complete();
+        if ($this->db->affected_rows() == '1') {
+            return TRUE;
+        } else {
+            // any trans error?
+            if ($this->db->trans_status() === FALSE) {
+                return 0;
+            }
+            return 1;
+        }
+    }
 }
