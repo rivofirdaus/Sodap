f9bdb891a66c83c3be1f127e985c3d185fddbb95
rivofirdaus <41298783+rivofirdaus@users.noreply.github.com>
2018-08-14 11:02:51 +0700

    Add files via upload

---
+++ application/modules/Kasi/views/v_list_laporan.php
@@ -0,0 +1,30 @@
+<section class="content-header">
+
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <div class="box box-warning">
+                <div class="box-header">
+                    <h3 class="box-title">Laporan kegiatan</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <table class="table table-bordered table-striped" id="tablekasi" style="width: 100%">
+                        <thead>
+                        <tr>
+                            <th width="50px">No</th>
+                            <th>nama kegiatan</th>
+                            <th>pagu dana</th>
+                            
+                            <th></th>
+							<th>aksi</th>
+							<th></th>
+                        </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
